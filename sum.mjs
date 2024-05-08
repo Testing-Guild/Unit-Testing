06e91d648a57aa6bf0893007e26965b2999b55ac
mahdi <mahdimamashli1383@gmail.com>
2024-05-08 16:02:47 +0330

    "Add error checking for non-numeric argument in sum function"
    This commit adds a check for non-numeric input in the `sum` function in the `sum.mjs` file. If the second argument (or if missing,

--- sum.mjs
+++ sum.mjs
@@ -1,3 +1,6 @@
 export function sum(a,b){
+    if (typeof (b||a) !== 'number') {
+        throw new TypeError('argument must be a number');
+      }
     return a+b
 }
--- test/sum.spec.mjs
+++ test/sum.spec.mjs
@@ -7,13 +7,12 @@ describe("sum", () => {
   });
 
   it("should throw an error for non-numeric second argument", () => {
-    try {
-      sum(1, "two");
-      assert.fail("Expected a number for the second argument");
-    } catch (error) {
-      assert.equal(error.name, assert.AssertionError.name);
-      assert.equal(error.message, "Expected a number for the second argument");
-    }
+    assert.throws(
+      () => sum(2, "one"),{
+        name:'TypeError',
+        message:"argument must be a number"
+      }
+    );
   });
   it("should return the sum of a positive integer and zero", () => {
     assert.equal(sum(20, 0), 20, "The sum of 20 and 0 should be 20");
